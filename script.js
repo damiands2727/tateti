410af0917b04353aed2bf147986eaa70573adfd9
Damiands <damiands2727@gmail.com>
2022-04-10 22:59:21 -0300

    test git

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+var array32=[1,2,3,4,8,9]
+console.log (array32)
+var otroarray = new Array (0,1,2,5,7);
+console.log(otroarray)
+
+var arrayCeldas = new Array(document.querySelectorAll("cuadradito"));
+console.log(arrayCeldas);
